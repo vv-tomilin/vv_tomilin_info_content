1d7ff51fea47d477935902b16eeb6e9d7159c0ac
vv_tomilin <backatom1@gmail.com>
2023-01-15 22:45:05 +0500

    Поправил шапку и ссылки

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Header from './components/Header';
 function App() {
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       <div className="global-container">
         <Routes>
           {/* <Route path="/" element={<Resume />} /> */}
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom';
 function Header() {
   return (
     <header className="header">
-      <div className="header-link bold mr-40">
+      {/* <div className="header-link bold mr-40">
         <Link className="header-link bold" to="/">
           Резюме
         </Link>
-      </div>
+      </div> */}
       <div className="header-link bold ml-40">
         <Link className="header-link bold" to="/portfolio">
           Портфолио
